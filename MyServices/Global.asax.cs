fc19155b693320cefad50af1cb114bcb503812db
benjcev <baykal.ucar@gmail.com>
2015-04-03 09:32:20 +0300

    Olmadı.

--- MyServices/Global.asax.cs
+++ MyServices/Global.asax.cs
@@ -23,7 +23,7 @@ namespace MyServices
         protected void Application_BeginRequest(object sender, EventArgs e)
         {
             HttpContext.Current.Response.AddHeader("Access-Control-Allow-Origin", "*");
-            if (HttpContext.Current.Request.HttpMethod == "OPTIONS")
+            if (HttpContext.Current.Request.HttpMethod == "OPTIONS ")
             {
                 HttpContext.Current.Response.AddHeader("Access-Control-Allow-Methods", "GET, POST");
                 HttpContext.Current.Response.AddHeader("Access-Control-Allow-Headers", "Authorization, Origin, Content-Type, Accept, X-Requested-With, ClientIP, AuthenticationToken");
